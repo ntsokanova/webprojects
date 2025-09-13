ef851ccc37478310e86eb11840843a7214a578e3
Nelly <Nelly@192.168.0.121>
2025-09-13 20:14:30 +0300

    Website updates.

--- neliws/js/util.js
+++ neliws/js/util.js
@@ -6,3 +6,20 @@ function jumpTo(targetId){
     targetEl.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"}); 
     targetEl.classList.add("focused");
 }
+
+function jumpToAlgoPart(targetId){
+    console.log("Method jumpToAlgoPart called with " + targetId);
+
+    Array.from(document.getElementsByClassName("focusedTest")).forEach((el) => el.classList.remove("focusedTest"));
+    const targetEl = document.querySelector(targetId);
+    targetEl.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"}); 
+    targetEl.classList.add("focusedTest");
+}
+
+function copyCode(targetId){
+    console.log("Method copyCode called with " + targetId);
+    const targetEl = document.querySelector(targetId);
+    var copyText = targetEl.innerText;
+    navigator.clipboard.writeText(copyText);
+
+}
